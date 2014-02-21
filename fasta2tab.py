bb36db747155582a2b0bf43c47c4aa8de9eb5ad0
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-02-21 00:44:20 -0500

    I was able to get fasta2tab to convert a fasta file to a tab file, using a limited subset of output attributes. The next step will be to calculate the appropriate attributes and write them to the tab file as well.

--- fasta2tab.py
+++ fasta2tab.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import random
 import string
+import csv
+
 from Bio import SeqIO
 
 # the character set for sequence UUIDs
@@ -8,11 +10,51 @@ charset = string.letters + string.digits
 # the number of characters to use for sequence UUIDs
 n_char = 8
 
-def fasta2tab_file(fname):
+def prune_germline_records(lst_seq_record):
+    return [record for record in lst_seq_record if
+        record.description[0] !='>']
+
+def parse_clip_attr(seq_record):
     '''
-    This function will process a single clip fasta file into a single tab file
-    appropriately named.
+    This function accepts a Bio.SeqRecord, and parses the
+    'description' into a dictionary of supplementary 'clip' attributes, with
+    which it then updates the attributes of the SeqRecord.
     '''
+    # splits description by '|' into key-value pair strings
+    lst_str_keyval = [keyval for keyval in seq_record.description.split('|')]
+    # splits each key value pair string by ':', and creates dictionary
+    try:
+        dict_clip_attr = dict([keyval.split(':',1) for keyval in lst_str_keyval
+            if len(keyval)>0])
+    except:
+        print(lst_str_keyval)
+        raise
+    # updates seq_record with those dictionary items
+    seq_record.__dict__.update(dict_clip_attr)
+    return seq_record
+
+def append_uuid(lst_seq_record):
+    '''
+    This function accepts a list of Bio.SeqRecord, computes a unique
+    identifier for each, and appends it to the 'SEQUENCE_ID' string of each
+    SeqRecord.
+    '''
+    n_id = len(lst_seq_record)
+    lst_uuid = get_n_uuid(n_id)
+    for iI, record in enumerate(lst_seq_record):
+        try:
+            record.SEQUENCE_ID += lst_uuid[iI]
+        except:
+            print(record)
+            raise
+    
+def tabname(fastaname):
+    '''
+    This function generates a suitable name for the tab file based on the name
+    of the fasta file.
+    '''
+    return fastaname.rpartition('.')[0] + '.tab'
+
 def read_fasta_file(fname_fasta):
     '''
     This function will read a clip fasta file, and return a list of some kind
@@ -22,52 +64,75 @@ def read_fasta_file(fname_fasta):
     SEQUENCE_ID
     SEQUENCE_GAP
     GERMLINE_GAP_DMASK
-    CLONE
+    CLONE_ID
     '''
     f = open(fname_fasta,'r')
     lst_seq_record = [record for record in SeqIO.parse(f,'fasta')]
-    n_id = len(lst_seq_record)
-    lst_uuid = get_n_uuid(n_id)
-    
     return lst_seq_record
 
-def write_tab_file(fname_fasta,lst_seq_record):
+tpl_cols = (
+    'SEQUENCE_ID',
+    'CLONE_ID',
+    )
+def write_tab_file(fname_tab,lst_seq_record):
     '''
     This function will take a list of Bio.SeqRecord instances, extract the
     needed sequence and metadata from each, and write them to an appropriately
     formatted tab file.
     '''
-    with open(fname_fasta,'w') as f:
+    with open(fname_tab,'w') as f:
         tab_writer = csv.writer(f, delimiter='\t')
         # write header
+        tab_writer.writerow(tpl_cols)
         for seq_record in lst_seq_record:
             # write row
-#            tab_writer.writerow([
-#                seq_record.SEQUENCE_ID,
-#                seq_record.SEQUENCE_GAP,
-#                seq_record.GERMLINE_GAP_DMASK,
-#                seq_record.CLONE,
-#                ])
+            tab_writer.writerow([seq_record.__dict__[field] for field
+                in tpl_cols])
             pass
 
 def get_n_uuid(n):
     '''
     This function will generate as many UUID as requested, confirming that
     they are unique.
-    It is neither elegant nor efficient.
     '''
     # generates list of UUID prior to checking for duplicates
     lst_uuid = [get_uuid() for iI in xrange(n)]
     # WARNING: while loop, remove in future versions
     # check to see that all UUID in lst_uuid are unique
-    while len(lst_uuid) > len(set(lst_uuid)):
-        lst_uuid = [get_uuid() for iI in xrange(n)]
-    return lst_uuid
+    while True:
+        lst_uuid = set(lst_uuid)
+        n_unique = len(lst_uuid)
+        if n_unique == n:
+            break
+        lst_uuid.update([get_uuid() for iI in xrange(n-n_unique)])
+    return list(lst_uuid)
+
 def get_uuid():
     '''
     This function returns a single UUID using module variables charset,n_char.
     '''
     return ''.join(random.choice(charset) for iI in xrange(n_char))
+
+def fasta2tab_file(fname):
+    '''
+    This function will process a single clip fasta file into a single tab file
+    appropriately named.
+    '''
+    #generate an initial list of SeqRecord
+    lst_seq_record = read_fasta_file(fname)
+    #prune out entries that list germlines
+    lst_seq_record = prune_germline_records(lst_seq_record)
+    #parse supplementary attributes
+    for record in lst_seq_record:
+        parse_clip_attr(record)
+    #append uuid portion to SEQUENCE_ID
+    append_uuid(lst_seq_record)
+    #find an appropriate name for the tab file
+    tabfname = tabname(fname)
+    #write the final list of SeqRecord to the tab file
+    write_tab_file(tabfname, lst_seq_record)
+    return None
+
 if __name__ == '__main__':
     import sys
     lst_fasta_files = sys.argv[1:]
