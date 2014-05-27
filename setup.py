66ea103d06efc2f1822a54b221bbe203a20d73e8
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2014-05-26 22:24:17 -0400

    Bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='fasta2tab',
-        version='0.1.0',
+        version='0.1.1',
         author='J. D. A. Gilliland',
         author_email='jdagilliland@gmail.com',
         py_modules=['fasta2tab'],
